a3983acd06c9dc324767607a8355244aeddb01d9
sohiearth <126925211+SoHiEarth@users.noreply.github.com>
2026-05-23 23:41:34 +0900

    feat: Remove defer on customer script

--- customer/script.js
+++ customer/script.js
@@ -1,12 +1,11 @@
-// Parse the username from the URL query parameters
 const urlParams = new URLSearchParams(window.location.search);
 const username = urlParams.get('username');
 
-// Display the username in the header
 if (username) {
     document.getElementById('username').textContent = username + 'さん';
 } else {
     document.getElementById('username').textContent = 'ゲストさん';
+    username = 'ゲスト';
 }
 
 function show_details(product_name) {
